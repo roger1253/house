089d61d3779f12f5ef578bd3ea0f632044364469
ChaselWu <minowu@foxmail.com>
2017-11-03 23:04:34 +0800

    Add styleRule

---
+++ src/Rules/styleRules.js
@@ -0,0 +1,16 @@
+const cssLoader = require('../Utils/cssLoader')
+
+module.exports = runner => {
+  const output = []
+  const loaders = cssLoaders(runner.options)
+
+  for (var extension in loaders) {
+    const loader = loaders[extension]
+    output.push({
+      test: new RegExp('\\.' + extension + '$'),
+      use: loader
+    })
+  }
+
+  return output
+}
