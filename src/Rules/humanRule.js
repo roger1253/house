a90d884668f52d6422415509c67a58c25cb82370
ChaselWu <minowu@foxmail.com>
2017-11-05 21:08:31 +0800

    Update humanRule

--- src/Rules/humanRule.js
+++ src/Rules/humanRule.js
@@ -1,7 +1,14 @@
 module.exports = runner => {
   return {
-    test: /vue-human[\/\\].*\.js$/,
+    test: /\.js$/,
     loader: 'babel-loader',
-    exclude: /vue-human[\/\\](node_modules)[\/\\].*/
+    include: [
+      runner.path.join(runner.options.rootPath, 'node_modules/vue-human'),
+      runner.path.join(runner.options.rootPath, 'node_modules/vue-human-icons')
+    ],
+    exclude: [
+      runner.path.join(runner.options.rootPath, 'node_modules/vue-human/node_modules'),
+      runner.path.join(runner.options.rootPath, 'node_modules/vue-human-icons/node_modules')
+    ]
   }
 }
