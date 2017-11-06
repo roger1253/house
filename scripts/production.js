40c5357c7f65a42b28d0f87aa749214a9082bac6
ChaselWu <minowu@foxmail.com>
2017-11-06 14:29:28 +0800

    Remove file

--- scripts/production.js
+++
@@ -1,8 +0,0 @@
-const ProductionRunner = require('../src/Runners/ProductionRunner')
-const path = require('path')
-
-const runner = new ProductionRunner({
-  appPath: path.join(process.cwd(), 'example')
-})
-
-runner.run()
